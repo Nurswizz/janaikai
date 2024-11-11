d02bca0d09b38ad1e8c1a8c88354d2fb6360db65
Nurswizz <nur646132@gmail.com>
2024-11-12 01:42:23 +0500

    first front

---
+++ frontend/src/components/Content.jsx
@@ -0,0 +1,23 @@
+import comics from '../assets/img/comics 1.png';
+
+const Content = () => {
+    return (
+        <div className='content'>
+            <img src={comics} alt="comics" />
+            <div className='black-container'>
+                <h1>JANAIQAI</h1>
+                <p>Иногда самые громкие крики о помощи остаются неуслышанными</p>
+            </div>
+            <div className='info'>
+                <p>
+                    Janaiqai comics - комикс, посвященный одной из острых социальных проблем в Центральной Азии. По статистике каждая 5 женщина и каждый 13 мужчина подвергались насилию в детстве. 
+                </p>
+                <button className='access-button'>
+                    <a href="#">Получить доступ к комиксу</a>
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Content;
