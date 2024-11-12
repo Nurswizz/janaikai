c4f29060b95ede7da5c1f6e058db17671725a28e
Nurswizz <nur646132@gmail.com>
2024-11-12 22:56:39 +0500

    landing

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -4,16 +4,20 @@ const Content = () => {
     return (
         <div className='content'>
             <img src={comics} alt="comics" />
-            <div className='black-container'>
-                <h1>JANAIQAI</h1>
-                <p>Иногда самые громкие крики о помощи остаются неуслышанными</p>
-            </div>
-            <div className='info'>
-                <p>
+            <div className='right-side'>
+                <div className='black-container'>
+                    <h1>JANAIQAI</h1>
+                    <p>Иногда самые громкие крики о помощи остаются неуслышанными</p>
+                </div>
+                <div className='subheading'>
+                    <p>
                     Janaiqai comics - комикс, посвященный одной из острых социальных проблем в Центральной Азии. По статистике каждая 5 женщина и каждый 13 мужчина подвергались насилию в детстве. 
-                </p>
-                <button className='access-button'>
-                    <a href="#">Получить доступ к комиксу</a>
+                    </p>
+                </div>
+                <button>
+                    <a href="#">
+                        Получить доступ к комиксу
+                    </a>
                 </button>
             </div>
         </div>
