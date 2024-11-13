47be3012ab5c60bef17f00f9b4c7c2a664bdc8dd
Nurswizz <nur646132@gmail.com>
2024-11-13 20:07:30 +0500

    comics pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,12 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Content from './components/Content'
 import Info from './components/Info'
-import Timeline from './components/Timeline'
 function App() {
   return (
     <>
       <Navbar />
       <Content />
       <Info />
-      <Timeline />
     </>
   )
 }
--- frontend/src/components/Info.jsx
+++ frontend/src/components/Info.jsx
@@ -3,6 +3,7 @@ import page_2 from '../assets/img/page2.png'
 
 const Info = () => {
     return (
+    <div className='info'>
     <div className='background-container'>
         <ul>
           <li>8 анонимных историй</li>
@@ -18,7 +19,47 @@ const Info = () => {
           <li>Странички о тревожности</li>
           <li>Стихотворение “Ало, Мам”</li>
         </ul>
-        
+    </div>
+    <img className='img1' src={page_1} alt="" />
+    <img className='img2'src={page_2} alt="" />
+    <div className="horizontal-line"></div>
+    <div className="timeline">
+      <div className="timeline-item">
+        <div className="vertical-line"></div>
+        <p>Первый созвон</p>
+        <span>15 сентября</span>
+      </div>
+      <div className="timeline-item">
+        <div className="vertical-line"></div>
+        <p>Рассылка анонимного опросника</p>
+        <span>15-30 сентября</span>
+      </div>
+      <div className="timeline-item">
+        <div className="vertical-line"></div>
+        <p>Сценарий</p>
+        <span>17 сентября</span>
+      </div>
+      <div className="timeline-item">
+        <div className="vertical-line"></div>
+        <p>Написание стиха</p>
+        <span>до 15 октября</span>
+      </div>
+      <div className="timeline-item">
+        <div className="vertical-line"></div>
+        <p>Иллюстрирование</p>
+        <span>до 5 ноября</span>
+      </div>
+      <div className="timeline-item">
+        <div className="vertical-line"></div>
+        <p>Создание сайта</p>
+        <span>5-15 ноября</span>
+      </div>
+      <div className="timeline-item">
+        <div className="vertical-line"></div>
+        <p>Выпуск</p>
+        <span>16 ноября</span>
+      </div>
+    </div>
     </div>
       );
 }
