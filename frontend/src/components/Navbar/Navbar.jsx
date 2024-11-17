5f17fc7548e62bdf170c5b4dc8d200896bce0f45
Nurswizz <nur646132@gmail.com>
2024-11-17 22:17:33 +0500

    navbar updated

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <ul>
-                <li><button><a href="#">Поддержать проект</a></button></li>
+                <li className='btn'><button><a href="#">Поддержать проект</a></button></li>
                 <li><a href="/articles">Статьи</a></li>
-                <li><a href="/"><img src={logo} alt="logo" loading='lazy'/></a></li>
+                <li className='lg'><a href="/"><img src={logo} alt="logo" loading='lazy'/></a></li>
                 <li><a href="/course">Курс</a></li>
                 <li><a  href="#contact">Связаться с нами</a></li>
             </ul>
