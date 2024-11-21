1d82fd828b4b41b80b83561a570ba1659428af6e
Nurswizz <nur646132@gmail.com>
2024-11-22 00:14:31 +0500

    responsivness final

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,32 @@
+import { useState } from 'react';
 import logo from '../../assets/img/logo.png';
 import './Navbar.css';
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <ul>
-                <li className='btn'><button><a href="#">Поддержать проект</a></button></li>
-                <li><a href="/articles">Статьи</a></li>
-                <li className='lg'><a href="/"><img src={logo} alt="logo" loading='lazy'/></a></li>
-                <li><a href="/course">Курс</a></li>
-                <li><a  href="/#contact">Связаться с нами</a></li>
-            </ul>
-        </div>
-    )
-}
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="navbar">
+      <span className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </span>
+      <ul className={menuOpen ? 'active' : ''}>
+        <li className="btn">
+          <button>
+            <a href="#">Поддержать проект</a>
+          </button>
+        </li>
+        <li><a href="/articles">Статьи</a></li>
+        <li className="lg"><a href="/"><img src={logo} alt="logo" loading="lazy" /></a></li>
+        <li><a href="/course">Курс</a></li>
+        <li><a href="/#contact">Связаться с нами</a></li>
+      </ul>
+    </div>
+  );
+};
 
 export default Navbar;
