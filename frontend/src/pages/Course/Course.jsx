20d4e3298e84eeb1e6cef4eefdf537c9e505fc22
Nurswizz <nur646132@gmail.com>
2024-12-03 22:48:41 +0500

    minor changes

--- frontend/src/pages/Course/Course.jsx
+++ frontend/src/pages/Course/Course.jsx
@@ -8,7 +8,7 @@ const Content = () => {
                 <h1>Курс по детской безопасности</h1>
             </div>
             <div className='qorgan'>
-                <p> <strong>Qorgan Project</strong> - это проект, посвященный предотвращению сексуализированного насилия над детьми, их курс будет полезен и детям и взрослым. Обязательно посмотрите бесплатные уроки! Посмотреть полный курс можно по ссылке: <br /> <a href="https://www.youtube.com/watch?v=xF8GTviihQ4&list=PLdpm5MUquwobOPmz4AmPgl36nvQcby5he"><i style={{fontWeight: 400}}>Ссылка на курс</i></a></p>
+                <p> <strong>Qorgan Project</strong> - это проект, посвященный предотвращению сексуализированного насилия над детьми, их курс будет полезен родителям и взрослым. Обязательно посмотрите бесплатные уроки! Посмотреть полный курс можно по ссылке: <br /> <a href="https://www.youtube.com/watch?v=xF8GTviihQ4&list=PLdpm5MUquwobOPmz4AmPgl36nvQcby5he"><i style={{fontWeight: 400}}>Ссылка на курс</i></a></p>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/xF8GTviihQ4?si=gM53DZeYkNmKuByr" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             <div className='jarqyn'>
