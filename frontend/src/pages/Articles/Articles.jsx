a8ac9ad572d909bc0ebdf9be771376058398463e
Nurswizz <nur646132@gmail.com>
2024-12-05 21:25:16 +0500

    loading

--- frontend/src/pages/Articles/Articles.jsx
+++ frontend/src/pages/Articles/Articles.jsx
@@ -13,50 +13,57 @@ import { errorMessages } from '../../utils/messages';
 const Articles = () => {
     const [articles, setArticles] = useState([]);
     const [responseMessage, setResponseMessage] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const getArticles = async () => {
             try {
-                const res = await axios.get(
-                    `${import.meta.env.VITE_BACKEND_URL}/admin/admin-panel/articles`
-                );
-                setArticles(res.data);
-                } catch (error) {
-                    errorMessages(error, setResponseMessage);
-                }
-            };
-            getArticles();
-        }, []);
+            const res = await axios.get(
+                `${import.meta.env.VITE_BACKEND_URL}/admin/admin-panel/articles`
+            );
+            setArticles(res.data);
+            } catch (error) {
+            errorMessages(error, setResponseMessage);
+            } finally {
+            setLoading(false);
+            }
+        };
+        getArticles();
+    }, []);
 
     return (
         <div className='articles'>
-            {articles.map((article) => {
-                return (
-                <Article 
-                    key={article._id}
-                    title={article.title}
-                    img={article.imageUrl}
-                    link={article.link} /> );
-                
-            })}
+            {loading ? (
+                <h1 className='load-text' style={{color: "black"}}>Loading...</h1>
+            ) : (
+                articles.map((article) => (
+                    <Article 
+                        key={article._id}
+                        title={article.title}
+                        img={article.imageUrl}
+                        link={article.link} 
+                    />
+                ))
+            )}
             {responseMessage && <p>{responseMessage}</p>}
         </div>
-    )
+    );
 }
 
 const Container = () => {
     return (
         <div className='header'>
             <div className='container'>  
-                    <img src={pencil} alt="pencil" />
-                    <h1>Статьи</h1>
+                <img src={pencil} alt="pencil" />
+                <h1>Статьи</h1>
             </div>
             <div className='icon'>
                 <img src={book} alt="" />
             </div>
-       </div>
+        </div>
     );
 }
+
 const ArticlesPage = () => {
     return (
         <>
@@ -64,7 +71,7 @@ const ArticlesPage = () => {
             <Container />
             <Articles />
         </>
-    )
+    );
 }
 
 export default ArticlesPage;
